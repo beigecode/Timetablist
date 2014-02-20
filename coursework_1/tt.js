87dba9c189b757e7ba850b255d99eeee510de5d3
beigecode <coxofpigeons@yahoo.com>
2014-02-20 10:26:20 +0000

    complete redesign

--- coursework_1/tt.js
+++ coursework_1/tt.js
@@ -1,27 +1,26 @@
 $(function(){
-	$('#fixedDetails').hide();
+	$('#fixedDetails').hide(); // hides the module description table on page load
+	
 	$('#getModule').click(function(){
-	$('td').empty();	
+	$('td').empty(); // clears the all the table cells from previous data
 		$.getJSON('https://tracker.napier.ac.uk/timetable/cw.pl',
 			{module:$('#module').val()},
-			function(r){
+			function(r){ //populates the module description table with JSON data
 				$('#moduleCode').text(r.SITS_MOD_CODE);
 				$('#moduleName').text(r.SITS_MOD_NAME);
 				$('#moduleLeader').text(r.SITS_MOD_LEADER);
 				$('#credit').text(r.SITS_MOD_CRDT);
 				var schoolname = r.SITS_DPT_CODE;
-             
-           
 
-                 for(var i=0;i<r.events.length;i++){        // adds event to the timetable
-          $('#'+r.events[i].slot).html('<b>'+ r.SITS_MOD_NAME +'</b>' + '<br>' + r.events[i].type + '<br>' + r.events[i].rooms	)
+                 for(var i=0;i<r.events.length;i++){        // adds module events to the timetable
+          $('#'+r.events[i].slot).html('<b>' + r.SITS_MOD_NAME + '</b>' + '<br>' + r.events[i].type + '<br>' + r.events[i].rooms	)
         }
 
 		
 		var url='https://tracker.napier.ac.uk/timetable/cw.pl?action=list';
 $(function(){
   $.getJSON(url,{module:$('#module').val()},
-      function(d){
+      function(d){ // replaces school code with its name
         $('#school').text(d.school[schoolname].name);
       }
   )
@@ -29,15 +28,6 @@ $(function(){
 
 
 		})
-		$('#fixedDetails').show(2000);
+		$('#fixedDetails').show(2000); // an animation effect for the module description table
 	})
 });
-
-
-
-	
-
-  /*/  
-               $('#'+r.events[0].slot).text('event0')   //add event to the timetable
-               $('#'+r.events[1].slot).text('event1')
-            /*/
